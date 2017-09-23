2c4c939298957a565fbb5b7a47c8f73e77fdc0b0
dsh2 <dsh2@me.com>
2017-09-23 17:10:41 +0200

    tmux-backgroud: switch other panes as well

--- bin/tmux-random-background-color.sh
+++ bin/tmux-random-background-color.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-tmux select-pane -P bg=\#$(hexdump -n 3 -e \"%02x\" /dev/urandom | tr abdef 110)
+[ -n "$1" ] && TARGET_TTY="-t $1"
+tmux select-pane $TARGET_TTY -P bg=\#$(hexdump -n 3 -e \"%02x\" /dev/urandom | tr abdef 110) 2>/dev/null
+exit 0
