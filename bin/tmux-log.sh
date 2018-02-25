30de18e1975a0856b9d8052b56f59af59cc94173
dsh2 <dsh2@me.com>
2018-02-25 19:57:58 +0100

    tmux-log.sh: tweak output with sepeartors and less tmux crap

--- bin/tmux-log.sh
+++ bin/tmux-log.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env zsh
+#!env zsh
 TMUX_LOG=~/.tmux-log
 
 setopt extendedglob
 setopt monitor
 setopt notify 
-usage="${0:t} [-e|--edit] [-s|--show] zsh-history-event-number"
+usage="${0:t} [-e|--edit] zsh-history-event-number"
 
 if ! zparseopts -D \
-    D=DEBUG -debug=DEBUG \
-    s=SHOW -show=SHOW \
     e=EDIT -edit=EDIT; \
 then
     print $usage
     exit 1
 fi
 
-log_num=$1
-shift
-if [[ -n $1 ]]; then
+if [[ $# < 1 ]]; then
     print $usage
     exit 1
+
 fi
+log_num=$1
+shift
 
 # Parse number of history event
 if [[ ! $log_num = <-> ]]; then
+    # TODO: Try to understand what I am doing here...
     log_num=${1[(ws:-:)1]}
     log_parm=${1[(ws:-:)2,-1]}
 fi
@@ -35,15 +35,18 @@ if [ ! -f $log_file ]; then
 fi
 # TODO: check if log_file is compressed 
 
-if [[ -n $SHOW ]]; then
-    print OUTPUT: ${$(cat $log_file | wc):gfs,  , ,:gs, ,/,} 
-    # "("$(du -sm $log_file)" MiB)"
-    head -100 $log_file
-fi
-
 if [[ -n $EDIT ]]; then
     # ${=EDITOR} $log_file
     vim $log_file
+    exit $?
 fi
 
+print SIZE: \
+    ${${$(wc $log_file):gfs,  , ,:gs, ,/,}[1,3]} \
+    "("${$(du -h --apparent-size $log_file)[1]}")"
+# print DIR: adapt cloc
+# TODO: fix the following line sepeartor to respect full screen width
+print -P ${(r:$((COLUMNS - 1))::\u2500:)}
+head -1000 $log_file | sed -e 's,$,,' -e '$ d' 
+print -P ${(r:$((COLUMNS - 1))::\u2500:)}
 exit 0
