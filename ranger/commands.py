4e82623da3d471a187e7773edab016699baf23d6
dsh2 <dsh2@me.com>
2018-03-21 10:21:23 +0100

    ranger: trying to bootstrap config

--- ranger/commands.py
+++ ranger/commands.py
@@ -44,7 +44,7 @@ class fzf_select(Command):
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command="find -L . \( -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
