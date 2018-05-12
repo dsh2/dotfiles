5d4ed5c70e56d5a6f21b392cf500ea32fde9e723
dsh2 <dsh2@me.com>
2018-05-12 12:24:23 +0200

    i3lock: brainstorming...

--- bin/i3lock-wrapper.sh
+++ bin/i3lock-wrapper.sh
@@ -4,6 +4,11 @@ revert() {
   xset dpms 0 0 0
 }
 trap revert HUP INT TERM
+pactl set-sink-mute 0 1
+rfkill block all
 xset +dpms dpms 5 5 5
 i3lock -n -c ff0000 -f -e
+# TODO:
+# -add i3-nag to rf unblock?
+# -re-attach HDMI?
 revert
