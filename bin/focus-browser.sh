28197dd21b8377dcfd48a068071c8a3db057206e
dsh2 <dsh2@me.com>
2018-06-01 16:30:11 +0200

    focus-browser: try focus for herbstluftwm

--- bin/focus-browser.sh
+++ bin/focus-browser.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #wmctrl -ai $(wmctrl -lp | grep $(pidof xfce4-terminal) | cut -d ' ' -f 1)
-wmctrl -a chrome
+# wmctrl -ai $(wmctrl -l | \grep -i chrome | head -1 | cut -d\  -f 1)
+herbstclient jumpto $(wmctrl -l | \grep -i chrome | head -1 | cut -d\  -f 1)
