f375775dbbc62f23cc3abd8ea660dfa778fe054c
dsh2 <dsh2@me.com>
2018-09-12 19:00:32 +0200

    handle_tag: add some comments

--- bin/herbstluftwm/handle_tag.sh
+++ bin/herbstluftwm/handle_tag.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env zsh
 tag=$(herbstclient tag_status | tr \\t \\n | dmenu -p "Rename/merge tag:") || exit 0
 if [[ "$tag]" =~ ^[-.:+#%!] ]]; then
+    # merge
     old_tag=$(herbstclient get_attr tags.focus.name)
     herbstclient use ${tag:1}
     herbstclient merge_tag $old_tag
 elif [[ "$tag]" =~ ^@ ]]; then
+    # add
     herbstclient add ${tag:1}
     herbstclient use ${tag:1}
 else
+    # rename
     herbstclient rename $(herbstclient get_attr tags.focus.name) $tag
 fi
