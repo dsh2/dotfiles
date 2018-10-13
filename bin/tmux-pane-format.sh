0be570d7ff24c87f1aef2b437195e8aabb2c0b30
dsh2 <dsh2@me.com>
2018-10-13 19:54:29 +0200

    tmux: testing new pane title

--- bin/tmux-pane-format.sh
+++ bin/tmux-pane-format.sh
@@ -1,13 +1,13 @@
 tmux set-window-option pane-border-format \
-'#{pane_id}'\
-'(#{pane_width}x#{pane_height})'\
-': '\
+'['\
 '#{pane_tty}'\
 ': '\
-'#{pane_current_command}(#{pane_pid}) '\
-'#{pane_current_path} '\
-'['\
-'E:#{pane_synchronized} '\
-'D:#{pane_dead} '\
-'R:#{pane_dead_status} '\
+'#{pane_current_command} (#{pane_pid})'\
+' '\
+'#{pane_current_path}'\
+' '\
+'#{pane_width}x#{pane_height}'\
+' '\
+'#{pane_id}'\
+'#{?pane_synchronized, SYNC,}'\
 ']'\
