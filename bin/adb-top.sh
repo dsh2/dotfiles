8869f0f4cb82b3c101f6a640affef34fbdecf4db
dsh2 <dsh2@me.com>
2019-11-29 06:31:34 +0100

    adb-top: make refresh run on client side to archive better alignment

--- bin/adb-top.sh
+++ bin/adb-top.sh
@@ -1,2 +1,8 @@
-set -x
-adb shell top -m $(($(tput lines) - 3))
+#!/bin/sh
+#set -x
+clear
+while :; do 
+    # adb shell top -n 1 -m $(($(tput lines) - 3)) | column -t 
+    adb shell top -n 1 -m $(($(tput lines) - 3)) 
+    sleep 2
+done
