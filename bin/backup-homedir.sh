3d2ffa8e753e84d07491dfb0e7babdfc455fb517
dsh2 <dsh2@me.com>
2020-03-17 15:13:15 +0100

    Add backup-homedir.sh

---
+++ bin/backup-homedir.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+backup_path="/mnt/BACKUP/$HOSTNAME/$USER"
+sudo mkdir -p $backup_path 
+sudo cp -al $backup_path $backup_path-$(date '+%F__%T')
+sudo rsync \
+	$rsync_options \
+	--exclude-from $HOME/.dotfiles/rsync-homedir-excludes.txt \
+	-ax $HOME/ $backup_path/
+echo "rsync = $?"
+echo "backup_path = $backup_path"
+echo "rsync_options = $rsync_options"
