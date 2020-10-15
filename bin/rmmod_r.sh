ef21ab3a36718155ef5d7371c1b1a8d930086119
dsh2 <dsh2@me.com>
2020-10-15 21:45:54 +0200

    Trying to remove kernel modules recursively

---
+++ bin/rmmod_r.sh
@@ -0,0 +1,26 @@
+#!env zsh
+
+# set -x 
+rm() {
+    local module=$1
+    local prefix=$2
+    # echo ENTER: $module
+    # [ -d /sys/module/$module/holders ] || return
+    setopt nullglob
+
+    local holder
+    for holder in /sys/module/$module/holders/*; do 
+	holder=${holder##*/}
+	# echo holder = $holder
+	# echo $module: holder = $holder
+	rm $holder $prefix:$holder
+	# echo $prefix:$holder
+	echo sudo modprobe --dry-run --verbose $holder
+    done
+    # echo EXIT: $module
+}
+
+[[ $# == 1 ]] || { echo "usage: $0 module"; exit 1; }
+
+module=$1
+rm $module $module
