27a54217e9717526c4c4798a8a66ff4271bd2fc4
dsh2 <dsh2@me.com>
2020-11-10 21:39:01 +0100

    ssh: add some sanity checks here and there for LocalCommand

--- ssh_host_config_template/local_command.sh
+++ ssh_host_config_template/local_command.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
-set -e
+# TODO: Set alarm to detect run-anway scripts
+ 
 local -r n=/dev/null
 
 ssh_notify() 
 {
-	local -r level=$1; shift # low, normal, critical
+	local -r level=${SSH_DEBUG:-$1}; shift # low, normal, critical
 	local -r msg="$*"
 	local app="ssh $R"
 	[ -z $SSH_GROUP ] || app="$app ($SSH_GROUP)"
@@ -15,36 +16,40 @@ ssh_notify()
 	
 	# TODO: Update to notify-send.py
 	# TODO: Add icon
-	notify-send -u $level -a $app "$msg"
+	notify-send -u $level -a $app -- "$msg"
 }
 
 check_remote_clock() 
 {
 	time_diff_ok=2 # seconds
-	time_diff=$[ $(ssh $R date -u '+%s') - $(date -u '+%s') ]
-	(( $time_diff > $time_diff_ok ||  $time_diff < -$time_diff_ok )) &&
+	integer -r time_diff=$[ $(ssh $R date -u '+%s') - $(date -u '+%s') ]
+	(( $time_diff > $time_diff_ok || $time_diff < -$time_diff_ok )) &&
 		ssh_notify critical "Remote clock deviates more than $time_diff_ok seconds (diff = $time_diff)."
 }
 
 mount_remote_fs() 
 {
-	local sshfs_opts="-C -o idmap=user -o transform_symlinks" 
+	local sshfs_opts="-o compression=yes -o idmap=user -o transform_symlinks" 
 	local mnt_point=$local_home/mnt/$remote_hostname_cmd
+	fusermount -u $mnt_point >&$n || true
 	mkdir -p $mnt_point || { ssh_notify critical "Failed to create mount point \"$mnt_point\"." ; return ; }
-	sshfs -C $R:/ $mnt_point || { ssh_notify critical "sshfs failed." ; return ; }
+	sshfs $=sshfs_opts $R:/ $mnt_point || { ssh_notify critical "sshfs failed." ; return ; }
 	ssh_notify low "Successfully mounted sshfs."
 
 	# Create second mount point with root access - if possible
 	[[ $( ssh $R sudo id -u ) = 0 ]] || return 
 	mnt_point=$mnt_point-ROOT
+	fusermount -u $mnt_point >&$n || true
 	mkdir -p $mnt_point || { ssh_notify critical "Failed to create mount point \"$mnt_point\"." ; return ; }
 
+	# Login via localhost over control connection to ensure graceful shutdown of 
+	# root-connection when disconnecting from host
 	local ssh_cmd="ssh -J $R"
 	ssh_cmd+=" -i $local_home/.ssh/hosts/$remote_hostname_cmd/id"
 	ssh_cmd+=" -o HostKeyAlias=ROOT-$R"
 	ssh_cmd+=" -o StrictHostKeyChecking=accept-new"
 
-	sshfs -o ssh_command=$ssh_cmd root@localhost:/ $mnt_point || 
+	sshfs $=sshfs_opts -o ssh_command=$ssh_cmd root@localhost:/ $mnt_point || 
 		{ ssh_notify critical "sshfs failed for root." ; return ; }
 	
 	ssh_notify low "Successfully mounted sshfs as root."
@@ -58,7 +63,7 @@ connect_tun()
 	local -r l_ip=10.0.$nw.1
 	local -r r_ip=10.0.$nw.2
 	
-	# Check if remote tun is availble
+	# Check if remote tun is available
 	if ! ssh $R test -e /sys/class/net/$tun; then
 		if ssh $R sudo sh <<< "
 			ip tuntap add dev $tun mode tun user $remote_username && 
@@ -81,7 +86,9 @@ connect_tun()
 
 main() 
 {
+	ssh_notify low "LocalCommand: $@ ($#@) debug=\"$SSH_DEBUG\""
 	(( $#@ >= 11 )) || return 
+
 	C_hash=$1
 	local_home=$2
 	remote_hostname=$3
@@ -93,12 +100,15 @@ main()
 	remote_username=$9; shift
 	local_tuntap=$9; shift
 	local_username=$9
+
 	# TODO: Check if adding '-p $remote_port' makes sense...
-	R="$remote_username@$remote_hostname"
+	R="$remote_username@$remote_hostname_cmd"
 	( umask 077; echo $R > /tmp/ssh )
 
-	sleep 0.1
-	ssh -qO check $R || { ssh_notify critical "Connection failed to multiplex." ; return 1 ; }
+	sleep 0.3
+	o=$( ssh -o BatchMode=yes -O check $R 2>&1 ) || 
+		{ ssh_notify critical "Connection failed to multiplex: \"${o//[[:cntrl:]]/}\"" ; return 1 ; }
+
 	check_remote_clock
 	mount_remote_fs
 	connect_tun
