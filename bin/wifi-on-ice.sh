06f5682eb40cd749796ca706abb81a839b0d9488
dsh2 <dsh2@me.com>
2020-12-31 02:36:00 +0100

    wifionice: add dhcpcd_hook

--- bin/wifi-on-ice.sh
+++ bin/wifi-on-ice.sh
@@ -1,65 +1,121 @@
-#!/bin/bash
+#!/bin/sh
 
+readonly N="/dev/null"
+readonly DNS="9.9.9.9"
 readonly curl="curl --connect-timeout 1 --silent"
+
 # readonly curl="curl --connect-timeout 1 --trace -"
-
 # set -x
-set -e
-set -u
 
-has() { for f in $@; do type $f &> /dev/null || return 1 ; done }
+# set -e
+# set -u
+
+has() { for f in $@; do type $f > /dev/null || return 1 ; done }
 log() { logger -s -t wifi-on-ice -- ${@:-No message.}; }
-die() { log "Error: $* Exiting..."; exit 1; }
-ssid() { iwgetid $1 -r ; }
+# TODO: Check if exit screws dhcpcd 
+die() { log "Error: $* Exiting..."; exit 244; }
+connected() { ping -W 1 -i 0.2 -c 2 $DNS >$N; return 1; }
+# connected() { ping -W 1 -i 0.2 -c 2 $DNS >$N; }
+
+install_me() {
+	src="$0"
+	dst="$1"
+	sudo install -pZ -o root -g root -m 0555 $src $dst || die "Install failed."
+	log "Successfully installed \"$src\" at \"$dst\"."
+	nr_install=$(( nr_install + 1 ))  
+}
 
 install() {
-	readonly dst_dir="/etc/NetworkManager/dispatcher.d"
-	[ -d $dst_dir ] || die "NetworkManager dispatcher drop-in directory not found. (\"$dst_dir\")"
-	readonly dst_path=$dst_dir/$(basename $0)
-	sudo cp $0 $dst_path
-	sudo chown root:root $dst_path
-	log "Successfully installed \"$0\" at \"$dst_path\"."
+	me=$(basename "$0")
+	nr_install=0
+
+	# NetworkManager
+	nm_dir="/etc/NetworkManager/dispatcher.d"
+	[ -d $nm_dir ] && install_me "$nm_dir/30-$me"  # 30: arbitrary ordering value
+
+	# dhclient 
+	dh_dir="/etc/dhcp/dhclient-exit-hooks.d"
+	[ -d $dh_dir ] &&  install_me "$dh_dir/$me" 
+
+	# dhcpcd
+	dhcpcd_dir="/lib/dhcpcd/dhcpcd-hooks"
+	[ -d $dhcpcd_dir ] &&  install_me "$dhcpcd_dir/30-$me"  # 30: arbitrary ordering value 
+	
 	exit 0
 }
 
+ssid() {
+	iface=$1
+	ssid=$(iwgetid $iface -r)
+	[ -z $ssid ] && ssid=$(iw dev $iface info | sed -nE 's:^.*ssid (.*)$:\1:p')
+	[ -z $ssid ] && die "Failed to acquire SSID."
+	echo $ssid
+}
+
+
 nm_dispatch() {
-	readonly iface=$1
-	readonly event=$2
-	readonly ssid=$(ssid $iface)
-	[[ -n $ssid ]] || return 
-	[[ $ssid = "WIFIonICE" ]] || { log "Ignoring wifi \"$ssid\"."; exit 0; }
-	[[ $event =~ (.*-change$|^up$) ]] && connect
+	iface=$1
+	event=$2
+	case $event in
+		(.*-change$|^up$) 
+			connect $iface
+			;;
+	esac
+}
+
+dhcpcd_hook() {
+	[ "$ifwireless" = 1 ] || return 0
+	case $reason in
+		(STATIC|INFORM?|REBOOT?) 
+			connect $interface
+			;;
+	esac
+}
+
+direct_connect() {
+	[ $# != 1 ] && die "Expected interface name as only parameter. (\"$@\")."
+	iface=$1
+	[ -h /sys/class/net/$iface ] || die "Interface \"$iface\" not found."
+	connect $iface
 }
 
 connect() {
+	connected && { log "DNS $DNS already available."; return 0; }
+	iface=$1
+	ssid=$(ssid $iface)
+	[ "$ssid" = "WIFIonICE" ] || { log "Ignoring wifi \"$ssid\"."; return 0; }
+
 	wi_ip=$(dig wifionice.de @172.18.0.1 +short +timeout=1) || die "Failed to lookup wifionice.de."
 	wi_url='http://'$wi_ip'/de/'
 	wi_token=$($curl $wi_url | xmllint --html --xpath 'string(//input[@name="CSRFToken"]/@value)' -)
-	[[ -n $wi_token ]] || die "Failed to acquire csrf-token."
+	[ -n "$wi_token" ] || die "Failed to acquire csrf-token."
 	echo "csrf-token = $wi_token"
 
 	$curl $wi_url -H 'Cookie: csrf='$wi_token --data 'login=true&CSRFToken='$wi_token
 
-	ping -W 1 -i 0.2 -c 2 9.9.9.9 && logger "wifi-on-ice is connected to CloudFlare." || logger "wifi-on-ice failed to connect to CloudFlare."
+	connected && logger "wifi-on-ice is connected to $DNS." || logger "wifi-on-ice failed to connect to $DNS."
 }
 
 main() {
 	# log "cmd_line = \"$@\""
 
 	has curl || die "Please install curl (https://curl.haxx.se/)"
-	has iw || die "Please install iw (https://mirrors.edge.kernel.org/pub/software/network/iw/)"
-	has xmllint || die "Please install libxmls (https://xmlsoft.org/)"
-	
-	[[ $# = 1 && $1 = "install" ]] && install  # no return
+	has iw iwgetid || die "Please install iw (https://mirrors.edge.kernel.org/pub/software/network/iw/)"
+	has xmllint || die "Please install libxml2-utils (http://xmlsoft.org/)"
 
-	readonly caller_name=$(basename -- "$(ps -o cmd= $(ps -o ppid= $$))")
-	# log "caller = $caller_name"
-	case $caller_name in
-		("nm-dispatcher")
+	[ $# = 1 -a $1 = "install" ] && { shift; install $@; }  # no return
+
+	caller=$(basename -- "$(ps -o cmd= $(ps -o ppid= $$))")
+	# log "caller = $caller"
+	# log "interface = $interface, reason = \"$reason\""
+	case $caller in
+		(nm-dispatcher)
 			nm_dispatch $@ ;;
+		# TODO: Add dhclient
+		(dhcpcd*)
+			dhcpcd_hook $@ ;;
 		(*)
-			[[ $# > 0 ]] && log "Ignoring command line (\"$@\")."
-			connect ;;
+			direct_connect $@ ;;
 	esac
 }
 
