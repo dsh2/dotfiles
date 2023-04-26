199756a9947ad0fac76e9375669a979947d3e171
Daniel Schaffrath <daniel.schaffrath@umlaut.com>
2023-04-26 22:24:09 +0200

    adb-run: check if apk exists

--- bin/adb-run.sh
+++ bin/adb-run.sh
@@ -3,6 +3,8 @@ unset PS4
 set -x
 set -e
 
+[ -e $1 ] || { echo "File not found."; exit 9; }
+
 pkg=$( aapt dump badging $1 | awk -F" " '/package/ {print $2}' | awk -F"'" '/name=/ {print $2}' )
 [[ -n $pkg ]]
 act=$( aapt dump badging $1 | awk -F" " '/launchable-activity/ {print $2}' | awk -F"'" '/name=/ {print $2}' )
